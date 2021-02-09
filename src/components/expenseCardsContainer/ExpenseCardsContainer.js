9723e90405bde17d4da9d09f7015e9d8403b9728
Ramez <rameznoshy@gmail.com>
2021-02-09 15:14:16 +0200

    feat: initial css, icon, project structure

---
+++ src/components/expenseCardsContainer/ExpenseCardsContainer.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import ExpenseCard from './expenseCard/ExpenseCard'
+// import PropTypes from 'prop-types'
+
+const ExpenseCardsContainer = props => {
+    const [expenses, setExpenses] = useState([
+        {
+            "id": "1",
+            "date": "12/12",
+            "note": "note1",
+            "category": "cat1",
+            "paymentType": "card1",
+            "amount": 10000
+        },
+        {
+            "id": "2",
+            "date": "13/13",
+            "note": "note2",
+            "category": "cat2",
+            "paymentType": "card2",
+            "amount": 20000
+        },
+        
+    ])
+
+    const expenseCards = expenses.map(expense => <ExpenseCard key={expense.id} expense={expense}></ExpenseCard>)
+
+    return (
+        <div>
+            {expenseCards}
+        </div>
+    )       
+}
+
+// ExpenseCardsContainer.propTypes = {
+
+// }
+
+export default ExpenseCardsContainer
+
