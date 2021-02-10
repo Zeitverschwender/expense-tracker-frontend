69421b81058d9d88c942a34c62b0dfdae4a864b7
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-10 12:52:15 +0200

    feat: Floating Action Button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
 import Home from "./containers/home/Home";
 
 import github from "./assets/images/iconmonstr-github-1.svg";
 import styles from "./App.module.scss";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#F88F01",
+    },
+    secondary: {
+      main: "#009ff7",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <span className={"material-icons " + styles.sidemenuButton}>menu</span>
-        <h1>EXPENSE TRACKER</h1>
-      </header>
-      <Home />
-      <footer className={styles.appFooter}>
-        <a
-          href="https://github.com/Zeitverschwender/expense-tracker-frontend"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.github}
-        >
-          <img src={github} alt="Github Logo" />
-        </a>
-      </footer>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <span className={"material-icons " + styles.sidemenuButton}>
+            menu
+          </span>
+          <h1>EXPENSE TRACKER</h1>
+        </header>
+        <Home />
+        <footer className={styles.appFooter}>
+          <a
+            href="https://github.com/Zeitverschwender/expense-tracker-frontend"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.github}
+          >
+            <img src={github} alt="Github Logo" />
+          </a>
+        </footer>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import ExpenseCardsContainer from '../../components/expenseCardsContainer/ExpenseCardsContainer'
+import React from "react";
+import PropTypes from "prop-types";
+import ExpenseCardsContainer from "../../components/expenseCardsContainer/ExpenseCardsContainer";
+import { Fab } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 
-const Home = props => {
-    return (
-        <ExpenseCardsContainer></ExpenseCardsContainer>
-    )
-}
+import styles from "./Home.module.scss";
 
-Home.propTypes = {
+const Home = (props) => {
+  return (
+    <React.Fragment>
+      <div className={styles.fab}>
+        <Fab color="secondary" size="large" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </div>
+      <ExpenseCardsContainer></ExpenseCardsContainer>
+    </React.Fragment>
+  );
+};
 
-}
+Home.propTypes = {};
 
-export default Home
-
+export default Home;
