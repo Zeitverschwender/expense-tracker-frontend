c71e7addad7a18ee8630b3a9984009d3c534d847
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-10 18:38:22 +0200

    feat: expense cards styling

--- src/components/expenseCardsContainer/expenseCard/ExpenseCard.js
+++ src/components/expenseCardsContainer/expenseCard/ExpenseCard.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import CreditCardIcon from "@material-ui/icons/CreditCard";
+import SubjectSharpIcon from "@material-ui/icons/SubjectSharp";
+
 import styles from "./ExpenseCard.module.scss";
+
+import dollar from "../../../assets/images/dollar.svg";
 
 const ExpenseCard = (props) => {
   return (
     <div className={styles.card}>
-      <div className={styles.amount}>$ {props.expense.amount}</div>
+      <div className={styles.firstRow}>
+        <div className={styles.amount}>
+          ${props.expense.amount.toLocaleString()}
+        </div>
+        <div className={styles.date}>{props.expense.date}</div>
+      </div>
       <div className={styles.category}>{props.expense.category}</div>
-      <div className={styles.details}>det </div>
-      <div className={styles.date}>{props.expense.date}</div>
-
-      <div className={styles.categoryColor}></div>
+      <div className={styles.iconsList}>
+        {props.expense.paymentType === "Cash" ? (
+          <img src={dollar} alt="Cash icon" />
+        ) : (
+          <CreditCardIcon />
+        )}
+        {props.expense.note && <SubjectSharpIcon />}
+      </div>
     </div>
   );
 };
