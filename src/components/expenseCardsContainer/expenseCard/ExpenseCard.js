0b50b063ea5990cb558248b5c2a40ddcfdb34e18
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-10 14:30:46 +0200

    style: auto formatting

--- src/components/expenseCardsContainer/expenseCard/ExpenseCard.js
+++ src/components/expenseCardsContainer/expenseCard/ExpenseCard.js
@@ -1,23 +1,20 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import styles from './ExpenseCard.module.scss'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./ExpenseCard.module.scss";
 
-const ExpenseCard = props => {
-    return (
-            <div className={styles.card}>
-                <div className={styles.amount}>$ {props.expense.amount}</div>
-                <div className={styles.category}>{props.expense.category}</div>
-                <div className={styles.details}>det </div>
-                <div className={styles.date}>{props.expense.date}</div>
-                
-            <div className={styles.categoryColor}></div>
-            </div>
-    )
-}
+const ExpenseCard = (props) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.amount}>$ {props.expense.amount}</div>
+      <div className={styles.category}>{props.expense.category}</div>
+      <div className={styles.details}>det </div>
+      <div className={styles.date}>{props.expense.date}</div>
 
-ExpenseCard.propTypes = {
+      <div className={styles.categoryColor}></div>
+    </div>
+  );
+};
 
-}
+ExpenseCard.propTypes = {};
 
-export default ExpenseCard
-
+export default ExpenseCard;
