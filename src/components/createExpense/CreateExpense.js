142a29ba1d0c7b0919ae689251c4c44b3723f2b7
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-11 18:50:30 +0200

    feat: create expense amount

--- src/components/createExpense/CreateExpense.js
+++ src/components/createExpense/CreateExpense.js
@@ -1,26 +1,40 @@
-import { InputAdornment, TextField } from "@material-ui/core";
-import React from "react";
+import {
+  InputAdornment,
+  TextField,
+} from "@material-ui/core";
+import React, { useState } from "react";
 
 import styles from "./CreateExpense.module.scss";
 
 const CreateExpense = React.forwardRef((props, ref) => {
   const [date, setDate] = useState(new Date());
+  const [amountError, setAmountError] = useState(false);
+  const [amount, setAmount] = useState(0);
+
+  const onAmountChange = (e) => {
+    e.preventDefault();
+    const newValue = e.target.value;
+    setAmountError(!newValue || newValue <= 0);
+    setAmount(parseFloat(newValue));
+  };
   return (
     <form className={styles.wrapper} autoComplete="off" ref={ref}>
       <TextField
+        classes={{ root: styles.gap }}
         required
-        error={false}
+        error={amountError}
         size="small"
         type="number"
         InputProps={{
           startAdornment: <InputAdornment position="start">$</InputAdornment>,
         }}
-        helperText="The Postive Amount you paid"
+        helperText={amountError ? "The Postive Amount you paid" : ""}
         id="new-expense-amount"
         label="Amount"
         variant="outlined"
         autoFocus
         margin="dense"
+        onChange={onAmountChange}
       />
     </form>
   );
