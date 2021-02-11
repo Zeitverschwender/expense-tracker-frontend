feee97dc949c133c5ea24f3a9d5f9162f01cdf24
Ramez <rameznoshy@gmail.com>
2021-02-11 14:59:44 +0200

    fix: checking for production for axios base url

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_IS_PROD ? process.env.REACT_APP_API_PROD_URL : process.env.REACT_APP_API_DEV_URL
+const BASE_URL = process.env.NODE_ENV==='production' ? process.env.REACT_APP_API_PROD_URL : process.env.REACT_APP_API_DEV_URL
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL
