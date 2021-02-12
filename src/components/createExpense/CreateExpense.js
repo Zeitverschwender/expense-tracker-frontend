86b297d3459d6d58a43951cba82a1498c5eadc05
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-12 10:29:54 +0200

    refactor: create expense amount

--- src/components/createExpense/CreateExpense.js
+++ src/components/createExpense/CreateExpense.js
@@ -4,10 +4,8 @@ import {
   FormControl,
   FormControlLabel,
   FormLabel,
-  InputAdornment,
   Radio,
   RadioGroup,
-  TextField,
 } from "@material-ui/core";
 import { MuiPickersUtilsProvider, DateTimePicker } from "@material-ui/pickers";
 import CreditCardIcon from "@material-ui/icons/CreditCard";
@@ -23,39 +21,18 @@ import PaymentMethod from "../../models/PaymentType";
 
 import dollar from "../../../src/assets/images/dollar.svg";
 import dollarOrange from "../../../src/assets/images/dollar_orange.svg";
+import AmountTextfield from "./formControls/amountTextfield/AmountTextfield";
 
 const CreateExpense = React.forwardRef((props, ref) => {
   const [date, setDate] = useState(new Date());
-  const [amountError, setAmountError] = useState(false);
   const [amount, setAmount] = useState(0);
-  const [isMoreInfoShown, setIsMoreInfoShown] = useState(false);
   const [paymentMethod, setPaymentMethod] = useState(PaymentMethod.CASH);
 
-  const onAmountChange = (e) => {
-    e.preventDefault();
-    const newValue = e.target.value;
-    setAmountError(!newValue || newValue <= 0);
-    setAmount(parseFloat(newValue));
-  };
+  const [isMoreInfoShown, setIsMoreInfoShown] = useState(false);
+
   return (
     <form className={styles.wrapper} autoComplete="off" ref={ref}>
-      <TextField
-        classes={{ root: styles.gap }}
-        required
-        error={amountError}
-        size="small"
-        type="number"
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-        }}
-        helperText={amountError ? "The Postive Amount you paid" : ""}
-        id="new-expense-amount"
-        label="Amount"
-        variant="outlined"
-        autoFocus
-        margin="dense"
-        onChange={onAmountChange}
-      />
+      <AmountTextfield setAmount={setAmount} gapClassname={styles.gap} />
       <div
         style={{ display: isMoreInfoShown ? "" : "none" }}
         className={styles.moreInfo}
