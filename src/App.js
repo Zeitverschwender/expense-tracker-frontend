67d8369d7c1fcf5ae132efd01331d7bc178bced7
Ramez <rameznoshy@gmail.com>
2021-02-12 16:22:30 +0200

    feat: handle routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
 import Home from "./containers/home/Home";
 
 import styles from "./App.module.scss";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Graphs from "./containers/graphs/Graphs";
+import Categories from "./containers/categories/Categories";
 
 const theme = createMuiTheme({
   palette: {
@@ -18,12 +21,22 @@ const theme = createMuiTheme({
 });
 
 
-function App() {
+const App = props => {
+  let routes = (
+    <Switch>
+      <Route path="/categories" component={Categories} />
+      <Route path="/graphs" render={Graphs} />
+      <Route path="/expenses" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Redirect to="/expenses" />
+    </Switch>
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <div className={styles.app}>
         <Header></Header>
-        <Home />
+        {routes}
         <Footer></Footer>
       </div>
     </ThemeProvider>
