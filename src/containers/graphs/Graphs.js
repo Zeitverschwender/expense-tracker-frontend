f9cf6b8f92b29c1e1744d696701d301554c26384
Ramez <rameznoshy@gmail.com>
2021-02-12 16:22:17 +0200

    chore: placeholder files for graph and categories routes

---
+++ src/containers/graphs/Graphs.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Graphs = props => {
+    return (
+        <div>
+            graphs
+        </div>
+    )
+}
+
+Graphs.propTypes = {
+
+}
+
+export default Graphs
