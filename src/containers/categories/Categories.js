103e19b4408b5ace5351bf54421d7cee2fdc9ba3
Ramez <rameznoshy@gmail.com>
2021-02-13 15:24:43 +0200

    feat: category card container

--- src/containers/categories/Categories.js
+++ src/containers/categories/Categories.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import CategoryCardsContainer from '../../components/CategoryCardsContainer/CategoryCardsContainer'
 
 const Categories = props => {
     return (
         <div>
-            cats
+            <CategoryCardsContainer></CategoryCardsContainer>
         </div>
     )
 }
