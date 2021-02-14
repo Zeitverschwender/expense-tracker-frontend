c1a208f2048e15445d4df19ea3b7d5062aa91e35
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-14 14:45:56 +0200

    feat: sort expense cards chronologically

--- src/store/reducers/expenseReducer.js
+++ src/store/reducers/expenseReducer.js
@@ -11,18 +11,26 @@ const initialState = {
     ...setNoError()
 };
 
+function getChronoSortedExpenses(expenses) {
+  return expenses.sort((first, second) => {
+    const firstDate = new Date(first.date);
+    const secondDate = new Date(second.date);
+    return firstDate < secondDate ? 1 : firstDate > secondDate ? -1 : 0;
+  });
+}
+
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.SET_EXPENSES:
             return {
                 ...state,
-                expenses: action.expenses,
+                expenses: getChronoSortedExpenses(action.expenses),
                 ...setNoError()
             };
         case actionTypes.ADD_EXPENSE:
             return {
                 ...state,
-                expenses: [action.expense, ...state.expenses],
+                expenses: getChronoSortedExpenses([action.expense, ...state.expenses]),
                 ...setNoError()
             };
         case actionTypes.REMOVE_EXPENSE:
@@ -38,7 +46,7 @@ const reducer = ( state = initialState, action ) => {
             expenses = [...expenses.slice(0, updatedIndex), action.expense, ...expenses.slice(updatedIndex + 1)];
             return {
                 ...state,
-                expenses: expenses,
+                expenses: getChronoSortedExpenses(expenses),
                 ...setNoError()
             };
 
