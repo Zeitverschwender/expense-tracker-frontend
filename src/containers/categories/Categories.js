7587eb6fb228b6dda880a197e337ba72acb99f4d
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-15 13:36:49 +0200

    style: auto formatting

--- src/containers/categories/Categories.js
+++ src/containers/categories/Categories.js
@@ -1,19 +1,14 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import CategoryCardsContainer from '../../components/CategoryCardsContainer/CategoryCardsContainer'
+import React from "react";
+import CategoryCardsContainer from "../../components/CategoryCardsContainer/CategoryCardsContainer";
 
-import styles from './Categories.module.scss';
+import styles from "./Categories.module.scss";
 
-const Categories = props => {
-    return (
-        <section className={styles.categoriesCardsContainer}>
-            <CategoryCardsContainer></CategoryCardsContainer>
-        </section>
-    )
-}
+const Categories = (props) => {
+  return (
+    <section className={styles.categoriesCardsContainer}>
+      <CategoryCardsContainer></CategoryCardsContainer>
+    </section>
+  );
+};
 
-Categories.propTypes = {
-
-}
-
-export default Categories
+export default Categories;
