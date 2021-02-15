14c8dbd0893ae584b3a8b11a53e7111f18ab85d9
Ramez <rameznoshy@gmail.com>
2021-02-15 21:11:27 +0200

    feat: welcome screen

---
+++ src/containers/welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Welcome.module.scss'
+
+const Welcome = () => {
+    return (
+        <h1 className={styles.center}>
+            Welcome to Expense Tracker!
+        </h1>
+    )
+}
+
+export default Welcome
