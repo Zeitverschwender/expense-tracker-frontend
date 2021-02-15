566ab5e7d849246911409531244e599294b39155
SamehAmnoun <samehamnoun@gmail.com>
2021-02-15 15:53:00 +0200

    feat: export base_url for use in other modules

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios';
 axios.defaults.withCredentials = true;
 
-const BASE_URL = process.env.NODE_ENV==='production' ? process.env.REACT_APP_API_PROD_URL : process.env.REACT_APP_API_DEV_URL
+export const BASE_URL = process.env.NODE_ENV==='production' ? process.env.REACT_APP_API_PROD_URL : process.env.REACT_APP_API_DEV_URL
 
 export default axios.create({
     baseURL: BASE_URL
