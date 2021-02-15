668e2caabf2436753402595095c4b1b2dcfb74f8
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-15 11:27:05 +0200

    style: auto formatting

--- src/components/CategoryCardsContainer/CategoryCardsContainer.js
+++ src/components/CategoryCardsContainer/CategoryCardsContainer.js
@@ -1,31 +1,37 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import CategoryCard from './categoryCard/CategoryCard'
-import * as actions from '../../store/actions/index';
+import React, { useEffect, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CategoryCard from "./categoryCard/CategoryCard";
+import * as actions from "../../store/actions/index";
 import styles from "./CategoryCardsContainer.module.scss";
-import CategoriesHeader from './categoriesHeader/CategoriesHeader';
+import CategoriesHeader from "./categoriesHeader/CategoriesHeader";
 
-const CategoryCardsContainer = props => {
-    const dispatch = useDispatch();
+const CategoryCardsContainer = (props) => {
+  const dispatch = useDispatch();
 
-    const categories = useSelector(state => state.categories.categories);
-    const hasError = useSelector(state => state.categories.hasError);
-    const error = useSelector(state => state.categories.error);
-    const errorDescription = useSelector(state => state.categories.errorDescription);
+  const categories = useSelector((state) => state.categories.categories);
+  const hasError = useSelector((state) => state.categories.hasError);
+  const error = useSelector((state) => state.categories.error);
+  const errorDescription = useSelector(
+    (state) => state.categories.errorDescription
+  );
 
-    const ongetAllCategories = useCallback(() => dispatch(actions.getAllCategories()), []);
-    useEffect(() => {
-        ongetAllCategories()
-    }, [ongetAllCategories])
+  const ongetAllCategories = useCallback(
+    () => dispatch(actions.getAllCategories()),
+    []
+  );
+  useEffect(() => {
+    ongetAllCategories();
+  }, [ongetAllCategories]);
 
-    const categoryCards = categories.map(cat => <CategoryCard key={cat._id} category={{...cat, note:'zoz'}}></CategoryCard>)
-    return (
-        <div className={styles.categoriesCardsList}>
-            <CategoriesHeader />
-                {categoryCards}
-        </div>
-    )  
-}
+  const categoryCards = categories.map((cat) => (
+    <CategoryCard key={cat._id} category={cat}></CategoryCard>
+  ));
+  return (
+    <div className={styles.categoriesCardsList}>
+      <CategoriesHeader />
+      {categoryCards}
+    </div>
+  );
+};
 
-export default CategoryCardsContainer
-
+export default CategoryCardsContainer;
