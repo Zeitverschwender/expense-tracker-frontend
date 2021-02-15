4442f8815cfe4596b59f7730aab224cadd64dc71
Ramez <rameznoshy@gmail.com>
2021-02-15 21:12:00 +0200

    feat: router and sidebar auth check

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
-import Home from "./containers/home/Home";
+import { useSelector } from 'react-redux';
 
 import styles from "./App.module.scss";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+
+import Home from "./containers/home/Home";
 import Graphs from "./containers/graphs/Graphs";
+import Welcome from "./containers/welcome/Welcome";
 import Categories from "./containers/categories/Categories";
 
 const theme = createMuiTheme({
@@ -24,21 +27,35 @@ const theme = createMuiTheme({
 
 
 const App = props => {
-  let routes = (
-    <Switch>
-      <Route path="/categories" component={Categories} />
-      <Route path="/graphs" component={Graphs} />
-      <Route path="/expenses" component={Home} />
-      <Route exact path="/" component={Home} />
-      <Redirect to="/expenses" />
-    </Switch>
-  );
+
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/categories" component={Categories} />
+        <Route path="/graphs" component={Graphs} />
+        <Route path="/expenses" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Redirect to="/expenses" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
 
   return (
     <ThemeProvider theme={theme}>
       <div className={styles.app}>
         <Header></Header>
-        {routes}
+          {routes}
         <Footer></Footer>
       </div>
     </ThemeProvider>
--- src/components/header/sidebar/navigationItem/NavigationItem.js
+++ src/components/header/sidebar/navigationItem/NavigationItem.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 
 const NavigationItem = props => {
+    const isLoggedIn = useSelector(state => state.user.isLoggedIn);
     const textCapitalized = props.toLink.charAt(0).toUpperCase() + props.toLink.slice(1);
     const IconComponent = props.icon
     return (
-        <ListItem button key={props.toLink} component={Link} to={props.toLink} onClick = {props.action()}>
+        <ListItem disabled={!isLoggedIn} button key={props.toLink} component={Link} to={props.toLink} onClick = {props.action()}>
             <ListItemIcon>
                 <IconComponent color='secondary'></IconComponent>
             </ListItemIcon>
